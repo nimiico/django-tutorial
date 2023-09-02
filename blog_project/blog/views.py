5f29d735ecfdb9be141cdb65eb1e1437fa03d098
nimiico <nimaeslami206@gmail.com>
2023-09-02 20:04:17 +0330

    03-Intermediates - create a blog page project with pycharm IDE

---
+++ blog_project/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+def posts(request):
+    return render(request, 'blog/all-posts.html' )
+
+def single_post(request, slug):
+    return render(request, 'blog/post-detail.html')

b369301c756f781e20bd6f4f9d6da252934e6163
nimiico <nimaeslami206@gmail.com>
2023-09-06 12:47:21 +0330

    04-Advance - Basics of database

---
+++ eshop_project/product_module/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=300)
+    price = models.IntegerField()
+    rating = models.IntegerField(validators=[MaxValueValidator(1), MaxValueValidator(5)], default=0)
+    short_description = models.CharField(max_length=360, null=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"
